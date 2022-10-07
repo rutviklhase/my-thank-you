1c4440b5ef95694efbe1ca8dd7f05ac5498faeed
Rutvik Lhase <Rutvik.Lhase@genmills.com>
2022-10-07 07:36:38 +0530

    true 8

--- dist/index.js
+++ dist/index.js
@@ -9690,19 +9690,26 @@ const github = __nccwpck_require__(5438);
 async function run()
 {
     console.log('Sanity check');
-    
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+    try{
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.issues.createComment({
+    await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
         body: 'Thank you for submitting a pull request! We will try to review this as soon as we can!'
     });
 
+    }
+
+    catch(error){
+        core.setFailed(error.message);
+
+    }
 }
 
 run();
