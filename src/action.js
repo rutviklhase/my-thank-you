133486e494a9bbc805ccc96afc8c1c24fe4ff42d
Rutvik Lhase <Rutvik.Lhase@genmills.com>
2022-10-07 07:02:19 +0530

    saying thank you to new pull requests

--- src/action.js
+++ src/action.js
@@ -1,6 +1,21 @@
+const core = require('@actions.core');
+const github = require('@actions/github');
+
 async function run()
 {
     console.log('Sanity check');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number:pull_request.number,
+        body: 'Thank you for submitting a pull request! We will try to review this as soon as we can!'
+    })
 
 }
+
 run();
